458e551477e8cb74c2d0b2b5f709599175d5ed4b
ederson23 <ederson@webfloat.com.br>
2023-11-08 18:51:29 -0300

    Entrega 2 finalizada

--- src/main/java/sistemasdistribuidos/servidor/ServerController.java
+++ src/main/java/sistemasdistribuidos/servidor/ServerController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.json.JSONObject;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -24,8 +25,6 @@ import java.util.concurrent.Executors;
 
 public class ServerController {
     private String SECRET_KEY = "AoT3QFTTEkj16rCby/TPVBWvfSQHL3GeEz3zVwEd6LDrQDT97sgDY8HJyxgnH79jupBWFOQ1+7fRPBLZfpuA2lwwHqTgk+NJcWQnDpHn31CVm63Or5c5gb4H7/eSIdd+7hf3v+0a5qVsnyxkHbcxXquqk9ezxrUe93cFppxH4/kF/kGBBamm3kuUVbdBUY39c4U3NRkzSO+XdGs69ssK5SPzshn01axCJoNXqqj+ytebuMwF8oI9+ZDqj/XsQ1CLnChbsL+HCl68ioTeoYU9PLrO4on+rNHGPI0Cx6HrVse7M3WQBPGzOd1TvRh9eWJrvQrP/hm6kOR7KrWKuyJzrQh7OoDxrweXFH8toXeQRD8=";
-    @FXML
-    private Label welcomeText;
 
     @FXML
     private TextField serverPort;
@@ -39,11 +38,8 @@ public class ServerController {
     private boolean acceptingConnections = true;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onConnectButtonClick() {
         ServerService serverService = new ServerService();
-        serverService.setOnSucceeded(event -> {
-            welcomeText.setText("Servidor Iniciado!");
-        });
 
         initializeUsers();
 
@@ -51,22 +47,21 @@ public class ServerController {
     }
 
     private void initializeUsers() {
-        int id = idCounter++;
-        //senhas 123456 para ambos
-        User exampleAdmin = new User(id, "Nome do admin", "admin@email.com", hash("E10ADC3949BA59ABBE56E057F20F883E"), true, "");
-        id++;
-        User exampleUser = new User(id, "Nome usuario default", "user@email.com", hash("E10ADC3949BA59ABBE56E057F20F883E"), false, "");
-        userList.add(exampleUser);
+        //123456
+        User exampleAdmin = new User(idCounter, "admin", "a", hash("E10ADC3949BA59ABBE56E057F20F883E"), true, "");
+        idCounter++;
+        User exampleUser = new User(idCounter, "usuario default", "user@email.com", hash("E10ADC3949BA59ABBE56E057F20F883E"), false, "");
+        idCounter++;
         userList.add(exampleAdmin);
+        userList.add(exampleUser);
     }
-
+    
     private String getResponse(String request) {
         JSONObject responseJson = new JSONObject();
-
+        
         try {
             JSONObject requestJson = new JSONObject(request);
 
-            // Check if the action is "login"
             if (requestJson.has("action") && requestJson.getString("action").equals("autocadastro-usuario")) {
                 responseJson =  getRegisterResponse(requestJson);
             }
@@ -79,7 +74,15 @@ public class ServerController {
             else if (requestJson.has("action") && requestJson.getString("action").equals("pedido-proprio-usuario")) {
                 responseJson = getLoggedUserData(requestJson);
             }
-            else {
+            else if (requestJson.has("action") && requestJson.getString("action").equals("listar-usuarios")) {
+                responseJson = getListUsers(requestJson);
+            }
+            else if (requestJson.has("action") && (requestJson.getString("action").equals("autoedicao-usuario") || requestJson.getString("action").equals("edicao-usuario"))) {   	
+                responseJson = getChangeUser(requestJson);
+            }
+            else if (requestJson.has("action") && (requestJson.getString("action").equals("excluir-proprio-usuario") || requestJson.getString("action").equals("excluir-usuario"))) {   	
+                responseJson = getDeleteUser(requestJson);
+            } else {
                 responseJson.put("action", requestJson.has("action"));
                 responseJson.put("error", true);
                 responseJson.put("message", "Ação desconhecida");
@@ -89,10 +92,133 @@ public class ServerController {
             System.out.println(e);
         }
         String response = responseJson.toString();
-        System.out.println("Respondendo ao cliente: " + response);
+        System.out.println("S-->C: " + response);
+        System.out.println("------------------------------------------------------------------");
         return response;
     }
 
+    private JSONObject getListUsers(JSONObject requestJson) {
+        JSONObject responseJson = new JSONObject();
+        System.out.println("Entrei Lista de usuarios");
+        try {
+        	
+        	JSONArray users = new JSONArray();
+        	JSONObject data = new JSONObject();
+        	
+
+        	responseJson.put("message", "Sucesso");
+            responseJson.put("error", false);
+            responseJson.put("action", "listar-usuarios");
+        	
+        	for(User user: userList) {
+        		JSONObject usuario = new JSONObject();
+        		usuario.put("id", user.getId());
+        		usuario.put("name", user.getName());
+        		usuario.put("type", user.getType());
+        		usuario.put("email", user.getEmail());
+        		users.put(usuario);
+        	}
+        	responseJson.put("data", data);
+        	data.put("users", users);        
+        }
+        catch(JSONException e) {
+            System.out.println(e.toString());
+        }
+        return responseJson;
+    }
+    
+    private JSONObject getChangeUser(JSONObject requestJson) {
+    	JSONObject responseJson = new JSONObject();
+    	System.out.println("Entrei Mudança de usuario");
+    	try {
+    		
+    		int id;
+    		JSONObject jsonData = requestJson.getJSONObject("data");
+    		
+    		if(requestJson.getString("action").equals("edicao-usuario")) {
+    			id = Integer.parseInt(jsonData.getString("user_id"));
+    			responseJson.put("action",requestJson.getString("action"));
+    		} else {
+    			id = Integer.parseInt(jsonData.getString("id"));
+    			responseJson.put("action",requestJson.getString("action"));
+    		}
+    		
+    		for(User user : userList) {
+    			if(user.getId() == id) {
+    				if(!jsonData.getString("name").isBlank() && !jsonData.isNull("name")) {
+    					user.setName(jsonData.getString("name"));
+    				}
+    				if(!jsonData.getString("email").isBlank() && !jsonData.isNull("email")) {
+    					user.setEmail(jsonData.getString("email"));
+    				}
+    				if(!jsonData.getString("password").isBlank() && !jsonData.isNull("password")) {
+    					String hspw = hash(jsonData.getString("password"));
+    					user.setPassword(hspw);
+    				}
+    				
+    				responseJson.put("error", false);
+    				responseJson.put("message", "Usuário atualizado com sucesso!");
+    				
+    				return responseJson;
+    			}
+    		}
+    		
+    		responseJson.put("error", true);
+			responseJson.put("message", "Usuário não encontrado!");
+			
+			return responseJson;
+    		
+    	}
+    	catch(JSONException e) {
+    		System.out.println(e.toString());
+    	}
+    	
+    	return responseJson;
+    }
+    
+    private JSONObject getDeleteUser(JSONObject requestJson) {
+    	JSONObject responseJson = new JSONObject();
+    	try {
+    		if(requestJson.getString("action").equals("excluir-usuario")) {
+    			for(User user : userList) {
+    				if(user.getId() == Integer.parseInt(requestJson.getJSONObject("data").getString("user_id"))) {
+    					userList.remove(user);
+
+    					responseJson.put("error", false);
+    					responseJson.put("message", "Usuário removido com sucesso!");
+    					responseJson.put("action", "excluir-usuario");
+
+    					return responseJson;
+    				}
+    			}
+                responseJson.put("error", true);
+                responseJson.put("message", "Usuário não encontrado!");
+                responseJson.put("action", "excluir-usuario");
+            } else {
+    			for(User user : userList) {
+                    String email = requestJson.getJSONObject("data").getString("email");
+                    String password = requestJson.getJSONObject("data").getString("password");
+                    String token = requestJson.getJSONObject("data").getString("token");
+
+                    if(user.getEmail().equals(email) && BCrypt.checkpw(password,user.getPassword()))
+                    {
+                        userList.remove(user);
+                        responseJson.put("error", false);
+                        responseJson.put("message", "Usuário removido com sucesso!");
+                        responseJson.put("action", "excluir-proprio-usuario");
+                        return responseJson;
+                    }
+    			}
+                responseJson.put("error", true);
+                responseJson.put("message", "Credenciais invalidas!");
+                responseJson.put("action", "excluir-proprio-usuario");
+            }
+    	} catch (JSONException e) {
+    		System.out.println(e.toString());
+    	}
+    	return responseJson;
+    }
+    
     private JSONObject getLogoutResponse(JSONObject requestJson) {
         JSONObject responseJson = new JSONObject();
         try {
@@ -113,14 +239,16 @@ public class ServerController {
             for (User user: userList) {
                 if(user.getToken().equals(token)) {
                     JSONObject data = new JSONObject();
-                    data.put("id",user.getId());
-                    data.put("name",user.getName());
-                    data.put("type",user.getType());
-                    data.put("email",user.getEmail());
+                    JSONObject jasonuser = new JSONObject();
+                    data.put("user", jasonuser);
+                    jasonuser.put("id", user.getId());
+                    jasonuser.put("name", user.getName());
+                    jasonuser.put("type", user.getType());
+                    jasonuser.put("email", user.getEmail());
 
                     responseJson.put("action", "pedido-proprio-usuario");
                     responseJson.put("error", false);
-                    responseJson.put("message", "Aqui estão seus dados:");
+                    responseJson.put("message", "Sucesso");
                     responseJson.put("data", data);
                     return responseJson;
                 }
@@ -145,10 +273,8 @@ public class ServerController {
             int id = idCounter++;
             System.out.println("PW: " + password+ " || HASH: " + hashedpw);
 
-            // Check if there is a user in the userList with the provided email and password
             for (User user : userList) {
                 if (user.getEmail().equals(email)) {
-                    // Return a successful login response with the user's token
                     JSONObject data = new JSONObject();
                     responseJson.put("action", "autocadastro-usuario");
                     responseJson.put("error", true);
@@ -158,7 +284,6 @@ public class ServerController {
                 }
             }
 
-            // If no matching user is found, register user and return success message
             User newUser = new User(id, name , email, hashedpw, false, generateToken(id,false));
             System.out.println(newUser.toString());
             userList.add(newUser);
@@ -171,16 +296,15 @@ public class ServerController {
         }
         return responseJson;
     }
+    
     private JSONObject getLoginResponse(JSONObject requestJson) {
         JSONObject responseJson = new JSONObject();
         try {
             String email = requestJson.getJSONObject("data").getString("email");
             String password = requestJson.getJSONObject("data").getString("password");
 
-            // Check if there is a user in the userList with the provided email and password
             for (User user : userList) {
                 if (user.getEmail().equals(email) && BCrypt.checkpw(password,user.getPassword()) ) {
-                    // Return a successful login response with the user's token
                     user.setToken(generateToken(user.getId(),user.isAdmin()));
                     JSONObject data = new JSONObject();
                     data.put("token", user.getToken());
@@ -194,7 +318,6 @@ public class ServerController {
                 }
             }
 
-            // If no matching user is found, return an error response
             responseJson.put("action", "login");
             responseJson.put("error", true);
             responseJson.put("message", "Credenciais incorretas");
@@ -212,7 +335,7 @@ public class ServerController {
     public String generateToken(int id, boolean isAdmin) {
         return Jwts.builder()
                 .claim("user_id", id)
-                .claim("isAdmin", isAdmin)
+                .claim("admin", isAdmin)
                 .setSubject(Integer.toString(id))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
@@ -226,11 +349,11 @@ public class ServerController {
                 protected Void call() {
                     int port = Integer.parseInt(serverPort.getText());
                     try (ServerSocket serverSocket = new ServerSocket(port)) {
-                        System.out.println("Servidor na porta " + port);
+                        System.out.println("Executando na porta " + port);
 
                         while (acceptingConnections) {
                             Socket clientSocket = serverSocket.accept();
-                            System.out.println("Cliente conectou");
+                            System.out.println("Cliente conectado");
 
                             threadPool.execute(() -> {
                                 try {
@@ -239,7 +362,7 @@ public class ServerController {
 
                                     String message;
                                     while ((message = reader.readLine()) != null) {
-                                        System.out.println("Recebida do cliente: " + message);
+                                        System.out.println("C-->S: " + message);
 
                                         String response = getResponse(message);
 
